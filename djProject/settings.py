ecb1679a9fb9606d9fceecfc2101d835e211b3ee
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-10-05 18:16:49 -0500

    full integrate Django/React/PostegreSQL

--- djProject/settings.py
+++ djProject/settings.py
@@ -134,7 +134,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
 
 STATICFILES_DIRS = [

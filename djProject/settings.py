0bc1dad071a134d972f303e89d4f4bd07e21750a
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-10-05 21:42:02 -0500

    fixed cors

--- djProject/settings.py
+++ djProject/settings.py
@@ -42,8 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 MIDDLEWARE = [
@@ -54,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'djProject.urls'

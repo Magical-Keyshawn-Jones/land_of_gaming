db2cb69f602831cbafbab42b458b99d3f73e73e6
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-10-09 15:05:43 -0500

    testing debug

--- djProject/settings.py
+++ djProject/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
+
+if DEBUG == None:
+    DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
